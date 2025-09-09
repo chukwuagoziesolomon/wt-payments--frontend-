b9b2eebc698b915565e5d70df0c554b18794578b
izaakwalz <chukwumaizaak@gmail.com>
2025-09-09 10:13:29 +0100

    chore: update project configuration and add dashboard layout components
    - Updated biome.json to use local schema and added "extends" for ultracite rules.
    - Modified package.json to include new scripts for linting and type checking, and updated dependencies.
    - Added GitHub Actions workflow for code quality checks and security audits.
    - Introduced new components for the dashboard layout, including sidebar and user dropdown.

---
+++ components/layouts.tsx/dashboard-layout.tsx
@@ -0,0 +1,44 @@
+// import UserDropdown from "@/components/user-dropdown";
+// import FeedbackDialog from "@/components/feedback-dialog";
+// import ContactsTable from "@/components/contacts-table";
+import { RiScanLine } from "@remixicon/react";
+// import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "./app-sidebar";
+import UserDropdown from "./user-dropdown";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset className="overflow-hidden px-4 md:px-6 lg:px-8">
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+          <div className="flex flex-1 items-center gap-2 px-3">
+            <SidebarTrigger className="-ms-4" />
+            <Separator
+              className="mr-2 data-[orientation=vertical]:h-4"
+              orientation="vertical"
+            />
+
+            <RiScanLine aria-hidden="true" size={22} />
+          </div>
+          <div className="ml-auto flex gap-3">
+            <UserDropdown />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 py-4 lg:gap-6 lg:py-6">
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
