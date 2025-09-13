9452ce0ac9eee8f03e20247eec7cec919395fbb2
izaakwalz <chukwumaizaak@gmail.com>
2025-09-13 17:13:23 +0100

    update

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,12 +1,12 @@
 export default function DashboardPage() {
-	return (
-		<>
-			<div className='grid auto-rows-min gap-4 md:grid-cols-3'>
-				<div className='bg-muted/50 aspect-video rounded-xl' />
-				<div className='bg-muted/50 aspect-video rounded-xl' />
-				<div className='bg-muted/50 aspect-video rounded-xl' />
-			</div>
-			<div className='bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min' />
-		</>
-	);
+  return (
+    <>
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+      </div>
+      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+    </>
+  );
 }
--- components/layouts.tsx/user-dropdown.tsx
+++ components/layouts.tsx/user-dropdown.tsx
@@ -1,55 +1,66 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Button } from '@/components/ui/button';
+import { RiLogoutBoxLine, RiSettingsLine, RiTeamLine } from "@remixicon/react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuGroup,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-
-import { RiSettingsLine, RiTeamLine, RiLogoutBoxLine } from '@remixicon/react';
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function UserDropdown() {
-	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant='ghost' className='h-auto p-0 hover:bg-transparent'>
-					<Avatar className='size-8'>
-						<AvatarImage
-							src='https://raw.githubusercontent.com/origin-space/origin-images/refs/heads/main/exp1/user_sam4wh.png'
-							width={32}
-							height={32}
-							alt='Profile image'
-						/>
-						<AvatarFallback>KK</AvatarFallback>
-					</Avatar>
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent className='max-w-64' align='end'>
-				<DropdownMenuLabel className='flex min-w-0 flex-col'>
-					<span className='truncate text-sm font-medium text-foreground'>Keith Kennedy</span>
-					<span className='truncate text-xs font-normal text-muted-foreground'>k.kennedy@originui.com</span>
-				</DropdownMenuLabel>
-				<DropdownMenuSeparator />
-				<DropdownMenuGroup>
-					<DropdownMenuItem>
-						<RiSettingsLine size={16} className='opacity-60' aria-hidden='true' />
-						<span>Account settings</span>
-					</DropdownMenuItem>
-					<DropdownMenuItem>
-						<RiTeamLine size={16} className='opacity-60' aria-hidden='true' />
-						<span>Affiliate area</span>
-					</DropdownMenuItem>
-				</DropdownMenuGroup>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem>
-					<RiLogoutBoxLine size={16} className='opacity-60' aria-hidden='true' />
-					<span>Sign out</span>
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
-	);
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="h-auto p-0 hover:bg-transparent" variant="ghost">
+          <Avatar className="size-8">
+            <AvatarImage
+              alt="Profile image"
+              height={32}
+              src="https://raw.githubusercontent.com/origin-space/origin-images/refs/heads/main/exp1/user_sam4wh.png"
+              width={32}
+            />
+            <AvatarFallback>KK</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="max-w-64">
+        <DropdownMenuLabel className="flex min-w-0 flex-col">
+          <span className="truncate font-medium text-foreground text-sm">
+            Keith Kennedy
+          </span>
+          <span className="truncate font-normal text-muted-foreground text-xs">
+            k.kennedy@originui.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <RiSettingsLine
+              aria-hidden="true"
+              className="opacity-60"
+              size={16}
+            />
+            <span>Account settings</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <RiTeamLine aria-hidden="true" className="opacity-60" size={16} />
+            <span>Affiliate area</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <RiLogoutBoxLine
+            aria-hidden="true"
+            className="opacity-60"
+            size={16}
+          />
+          <span>Sign out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
