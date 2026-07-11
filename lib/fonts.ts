5d000f5ea1ab6c466b52edcd62a874b1b303b401
izaakwalz <chukwumaizaak@gmail.com>
2026-07-12 00:00:00 +0100

    chore: project setup

---
+++ lib/fonts.ts
@@ -0,0 +1,11 @@
+import { Mulish, Geist_Mono } from 'next/font/google';
+
+export const mulishSans = Mulish({
+	subsets: ['latin'],
+	variable: '--font-mulish-sans',
+});
+
+export const geistMono = Geist_Mono({
+	subsets: ['latin'],
+	variable: '--font-geist-mono',
+});
