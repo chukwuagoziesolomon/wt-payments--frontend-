0a9a67d5b88528ebd9f043dd10a4edc80ba8d644
Ofodile franklin <chukwuagoziesolomon@users.noreply.github.com>
2026-09-08 21:02:00 +0100

    first commit

--- components/ccc-provider.tsx
+++ components/ccc-provider.tsx
@@ -1,28 +1,28 @@
 "use client";
 
 import { ccc } from "@ckb-ccc/connector-react";
+import { cccNetwork } from "@/lib/ccc-config";
 
 export default function CccProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <ccc.Provider
-      name="WT Payments"
-      preferredNetworks={[
-        {
+  const preferredNetwork =
+    cccNetwork === "mainnet"
+      ? {
           addressPrefix: "ckb",
           signerType: ccc.SignerType.CKB,
           network: "mainnet",
-        },
-        {
+        }
+      : {
           addressPrefix: "ckt",
           signerType: ccc.SignerType.CKB,
           network: "testnet",
-        },
-      ]}
-    >
+        };
+
+  return (
+    <ccc.Provider name="WT Payments" preferredNetworks={[preferredNetwork]}>
       {children}
     </ccc.Provider>
   );
